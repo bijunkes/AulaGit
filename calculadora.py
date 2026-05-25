ccede978f8f5edf11f8128744cfd2e625d16c2e4
bijunkes <bianca.jr2007@gmail.com>
2026-05-25 15:14:27 -0300

    classe calculadora

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+def soma(a, b):
+    return print(a+b)
+
+def subtracao(a, b):
+    return print(a-b)
+
+def multiplicacao(a, b):
+    return print(a*b)
+
+def divisao(a, b):
+    return print(a/b)
+
+soma(2, 3)
+subtracao(2, 3)
+multiplicacao(2, 3)
+divisao(2, 3)
--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-def soma(a, b):
-    return print(a+b)
-
-def subtracao(a, b):
-    return print(a-b)
-
-def multiplicacao(a, b):
-    return print(a*b)
-
-def divisao(a, b):
-    return print(a/b)
-
-soma(2, 3)
-subtracao(2, 3)
-multiplicacao(2, 3)
-divisao(2, 3)
+import calculadora as c
+print(c.soma(2, 3))
+print(c.subtracao(2, 3))
